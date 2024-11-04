1bab12b735fcf997b0c7fd59e01b13557fb189cf
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-11-04 10:25:03 -0300

    fulk

--- Fluxo_Rede/fulk.py
+++ Fluxo_Rede/fulk.py
@@ -7,30 +7,24 @@ class Grafo:
         self.grafo = defaultdict(dict)
     
     def adicionar_aresta(self, u, v, capacidade):
-        # Adiciona uma aresta ao grafo
-        if u not in self.grafo:
-            self.grafo[u] = {}
-        if v not in self.grafo:
-            self.grafo[v] = {}
-
-        # Define a capacidade da aresta de u para v
-        self.grafo[u][v] = capacidade
+        # Adiciona uma aresta ao grafo com capacidade dada
+        if v not in self.grafo[u]:
+            self.grafo[u][v] = capacidade
+        else:
+            self.grafo[u][v] += capacidade  # Caso a aresta já exista, soma a capacidade
 
         # Certifica-se de que a aresta reversa existe com capacidade inicial 0
-        if v not in self.grafo[u]:
-            self.grafo[v][u] = 0
         if u not in self.grafo[v]:
-            self.grafo[v][u] = 0  # Inicializa a capacidade reversa com 0, se não existir
+            self.grafo[v][u] = 0
 
     def _DFS(self, fonte, alvo, caminho, visitado):
-        # Realiza a Busca em Profundidade (DFS) para encontrar um caminho aumentante
+        # Realiza a DFS para encontrar um caminho aumentante
         visitado.add(fonte)
         if fonte == alvo:
             return caminho
         
         for vizinho, capacidade in self.grafo[fonte].items():
             if vizinho not in visitado and capacidade > 0:
-                # Tenta encontrar um caminho aumentante
                 resultado = self._DFS(vizinho, alvo, caminho + [(fonte, vizinho)], visitado)
                 if resultado is not None:
                     return resultado
@@ -51,11 +45,7 @@ class Grafo:
             # Atualiza as capacidades residuais das arestas e suas reversas
             for u, v in caminho:
                 self.grafo[u][v] -= fluxo_aumentante
-                if v not in self.grafo[u]:
-                    self.grafo[u][v] = 0  # Inicializa se não existir
-                if u not in self.grafo[v]:
-                    self.grafo[v][u] = 0  # Inicializa se não existir
-                self.grafo[v][u] += fluxo_aumentante
+                self.grafo[v][u] += fluxo_aumentante  # Capacidade residual da reversa
             
             # Incrementa o fluxo total
             fluxo_maximo += fluxo_aumentante
@@ -65,11 +55,11 @@ class Grafo:
 
 # Exemplo de uso
 grafo = Grafo(4)
-grafo.adicionar_aresta('s', 'a', 15)
-grafo.adicionar_aresta('s', 'b', 12)
-grafo.adicionar_aresta('a', 'b', 15)
-grafo.adicionar_aresta('a', 't', 15)
-grafo.adicionar_aresta('b', 't', 15)
+grafo.adicionar_aresta('s', 'a', 2)
+grafo.adicionar_aresta('s', 'b', 2)
+grafo.adicionar_aresta('a', 'b', 2)
+grafo.adicionar_aresta('a', 't', 1)
+grafo.adicionar_aresta('b', 't', 5)
 
 # Calcular o fluxo máximo de 's' para 't'
 fluxo_maximo = grafo.calcular_fluxo_maximo('s', 't')
